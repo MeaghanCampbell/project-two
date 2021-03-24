e876afed9d2f239e98fe3a850d295fa5abd9aabb
Meaghan Campbell <emeaghancampbell@gmail.com>
2021-03-23 22:14:24 -0500

    initialize app with server

---
+++ controllers/api/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+// const userRoutes = require('./user-routes');
+// const sessionRoutes = require('./session-routes')
+// const benchmarkRoutes = require('./benchmark-routes')
+
+// router.use('/users', userRoutes);
+// router.use('/sessions', sessionRoutes)
+// router.use('/benchmarks', benchmarkRoutes)
+
+module.exports = router;
