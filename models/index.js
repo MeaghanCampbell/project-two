c79269470268ecee4ac02fba75b66ab4b50737dc
Meaghan Campbell <emeaghancampbell@gmail.com>
2021-03-26 09:25:25 -0500

    like model

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@ const User = require('./User');
 const Session = require('./Session');
 const Benchmark = require('./Benchmark');
 const Follow = require('./Follow')
+const Like = require('./Like')
 
 User.hasMany(Session, {
     foreignKey: 'user_id'
@@ -20,12 +21,14 @@ Benchmark.belongsTo(User, {
     foreignKey: 'user_id'
 })
 
+// User can follow many accounts
 User.hasMany(Follow, {
     foreignKey: 'user_id'
 })
 
+// follow belongs to a user
 Follow.belongsTo(User, {
     foreignKey: 'user_id' 
 })
 
-module.exports = { User, Session, Benchmark, Follow };
+module.exports = { User, Session, Benchmark, Follow, Like };
