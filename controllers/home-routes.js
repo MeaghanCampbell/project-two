daeebb86f6149755d7ea465fd030ab7ea53f4aa0
Meaghan Campbell <emeaghancampbell@gmail.com>
2021-03-26 14:47:53 -0500

    homepage & routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,17 +1,32 @@
 const router = require('express').Router()
+const sequelize = require('../config/connection');
+const { User, Session, Benchmark, Kudos, Follow } = require('../models');
 
 router.get('/', (req, res) => {
-    res.render('homepage', {
-        id: 1,
-        category: 'Bouldering',
-        time: 60,
-        level: 'Advanced',
-        date: '03/20/21',
-        description: 'Here is the workout description!',
-        kudos_count: 10,
-        user: {
-        username: 'test_user'
-    }
+    Session.findAll({
+        attributes: [
+            'id',
+            'date',
+            'category',
+            'time',
+            'level',
+            'description',
+            [sequelize.literal('(SELECT COUNT(*) FROM kudos WHERE session.id = kudos.session_id)'), 'kudos_count']
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbSessionData => {
+        const sessions = dbSessionData.map(session => session.get({ plain: true }))
+        res.render('homepage', { sessions })
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
     })
 })
 
