85470791b30f7e1eb790c4cd6cf6701ae869a92e
jojoford <75343703+jojoford@users.noreply.github.com>
2021-04-04 16:14:53 -0500

    Update index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,11 +5,13 @@ const workoutRoutes = require('./workout-routes')
 const benchmarkRoutes = require('./benchmark-routes')
 const followRoutes = require('./follow-routes')
 const searchRoutes = require('./search-routes')
+const savedWorkoutsRoutes = require('./saved-workouts-routes')
 
 router.use('/users', userRoutes);
 router.use('/workouts', workoutRoutes)
 router.use('/benchmarks', benchmarkRoutes)
 router.use('/follows', followRoutes)
 router.use('/searches', searchRoutes)
+router.use('/saved-workouts', savedWorkoutsRoutes)
 
 module.exports = router;
